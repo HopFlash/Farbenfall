a646ace58c730f56c727b9a8f3e0cd4fb8a87f2c
HopFlash <hopflash@gmx.de>
2021-01-11 21:28:53 +0100

    init a game window and show a background

--- src/main.py
+++ src/main.py
@@ -5,15 +5,51 @@ https://itch.io/jam/my-first-game-jam-winter-2021
 """
 
 import pygame
-# from pygame.locals import *
+from pygame.locals import QUIT
+
+
+class Intro(object):
+    """
+    docstring
+    """
+
+    def __init__(self, gameobj):
+        self.show = True
+        self.gameobj = gameobj
+
+    def render(self):
+        background = pygame.Surface(self.gameobj.screen.get_size())
+        background = background.convert()
+        background.fill((150, 250, 250))
+        self.gameobj.screen.blit(background, (0, 0))
+
 
 class Game(object):
     """
     docstring
     """
+
     def __init__(self):
         pygame.init()
+#        displayFlags = pygame.FULLSCREEN | pygame.RESIZABLE
+        displayFlags = pygame.RESIZABLE
+        self.screen = pygame.display.set_mode((800, 600), flags=displayFlags)
+        pygame.display.set_caption('Farbenfall v0.1')
+        self.clock = pygame.time.Clock()
+
+        self.intro = Intro(self)
+
+    def run(self):
+
+        while 1:
+            self.intro.render()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+            pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == "__main__":
     myGame = Game()
+    myGame.run()
