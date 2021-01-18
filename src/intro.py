08c4931e64ea9ac05f8e73fa1d897896227f3e3d
HopFlash <hopflash@gmx.de>
2021-01-18 21:40:48 +0100

    event system and intro functionallity
    using the pygame event system to switch from intro to game by using buttons in intro screen

--- src/intro.py
+++ src/intro.py
@@ -1,46 +1,123 @@
 import pygame
+from pygame.constants import SRCALPHA
+from pygame.sprite import AbstractGroup
 
 
-class Intro(object):
+class Intro(pygame.sprite.Sprite):
+    def __init__(self, gameobj, *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.gameobj = gameobj
+
+        self.image = pygame.Surface(self.gameobj.screen.get_size())
+        self.image = self.image.convert()
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        self.myGroup = pygame.sprite.RenderPlain()
+
+        self.introTitle = IntroTitle(gameobj, self, self.myGroup)
+        self.introButtons = [
+            IntroPlayButton(self.gameobj, self, 'Play', self.myGroup),
+            IntroAboutButton(self.gameobj, self, 'About', self.myGroup),
+            IntroQuitButton(self.gameobj, self, 'Quit', self.myGroup)
+        ]
+        for i, iButton in enumerate(self.introButtons):
+            iButton.rect.top = 400
+            iButton.rect.left = 150 + i * 150
+
+    def update(self, *args, **kwargs) -> None:
+        self.image = pygame.Surface(self.gameobj.screen.get_size())
+        self.image = self.image.convert()
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        self.myGroup.update()
+        self.myGroup.draw(self.image)
+
+    def checkMouseClick(self, event):
+        mouseClickPos = pygame.mouse.get_pos()
+        for iButton in self.introButtons:
+            if iButton.rect.collidepoint(mouseClickPos):
+                iButton.clicked(event)
+
+
+class IntroTitle(pygame.sprite.Sprite):
     """
     docstring
     """
+    colorList = [
+        (148, 0, 211),
+        (148, 0, 211),
+        (75, 0, 130),
+        (0, 0, 255),
+        (0, 255, 0),
+        (0, 255, 0),
+        (255, 255, 0),
+        (255, 127, 0),
+        (255, 0, 0),
+        (255, 0, 0)
+    ]
 
-    def __init__(self, gameobj):
-        self.show = True
+    def __init__(self, gameobj, parent, *groups: AbstractGroup):
+        super().__init__(*groups)
         self.gameobj = gameobj
+        self.parent = parent
         self.font = pygame.font.Font(None, 128)
-        self.text = self.font.render('Farbenfall', True, (200, 50, 0))
-        self.textStr = 'Farbenfall'
-        colorList = [
-            (148, 0, 211),
-            (148, 0, 211),
-            (75, 0, 130),
-            (0, 0, 255),
-            (0, 255, 0),
-            (0, 255, 0),
-            (255, 255, 0),
-            (255, 127, 0),
-            (255, 0, 0),
-            (255, 0, 0)
-        ]
-        self.introTextImages = []
+
+        introTextImages = []
         textOffset = 0
-        for i, c in enumerate(self.textStr):
-            charColor = colorList[i]
+        introTextHeight = 0
+        for i, c in enumerate('Farbenfall'):
+            charColor = self.colorList[i]
             charImage = self.font.render(c, True, charColor)
-            self.introTextImages.append((textOffset, charImage))
+            introTextImages.append((textOffset, charImage))
             textOffset = textOffset + charImage.get_width() + 3
-        self.introTextLength = textOffset
+            if charImage.get_height() > introTextHeight:
+                introTextHeight = charImage.get_height()
+        introTextWidth = textOffset
 
-    def render(self):
-        background = pygame.Surface(self.gameobj.screen.get_size())
-        background = background.convert()
-        background.fill((255, 255, 255))
+        self.image = pygame.Surface((introTextWidth, introTextHeight), flags=SRCALPHA)
+        self.rect = self.image.get_rect()
+        for charOffsetX, textImage in introTextImages:
+            self.image.blit(textImage, (charOffsetX, 0))
+        self.rect.centerx = round(self.parent.rect.centerx)
+        self.rect.centery = round(self.parent.rect.centery)
 
-        for charOffsetX, textImage in self.introTextImages:
-            textpos = textImage.get_rect(x=background.get_width() / 2 - self.introTextLength / 2 + charOffsetX,
-                                         centery=background.get_height() / 2)
-            background.blit(textImage, textpos)
+    def update(self):
+        self.rect.centerx = round(self.parent.rect.centerx)
+        self.rect.centery = round(self.parent.rect.centery)
 
-        self.gameobj.screen.blit(background, (0, 0))
+
+class IntroButton(pygame.sprite.Sprite):
+    def __init__(self, gameobj, parent, text, *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.gameobj = gameobj
+        self.parent = parent
+        self.text = text
+
+        self.font = pygame.font.Font(None, 40)
+
+        self.image = pygame.Surface((100, 50), flags=SRCALPHA)
+        self.image.fill((0, 0, 255))
+        self.rect = self.image.get_rect()
+        textImage = self.font.render(self.text, True, (255, 255, 0))
+        textRect = textImage.get_rect()
+        textRect.center = self.rect.center
+        self.image.blit(textImage, textRect)
+
+
+class IntroPlayButton(IntroButton):
+    def clicked(self, event):
+        playEvent = pygame.event.Event(self.gameobj.eventDict['INTRO_CLICKED'], action='PLAY')
+        pygame.event.post(playEvent)
+
+
+class IntroAboutButton(IntroButton):
+    def clicked(self, event):
+        playEvent = pygame.event.Event(self.gameobj.eventDict['INTRO_CLICKED'], action='ABOUT')
+        pygame.event.post(playEvent)
+
+
+class IntroQuitButton(IntroButton):
+    def clicked(self, event):
+        pygame.event.post(pygame.event.Event(pygame.QUIT))
--- src/main.py
+++ src/main.py
@@ -11,6 +11,13 @@ import block
 import waterfall
 
 
+def initUserEvents():
+    eventDict = {
+        'INTRO_CLICKED': pygame.event.custom_type(),
+    }
+    return eventDict
+
+
 class Game(object):
     """
     docstring
@@ -23,8 +30,16 @@ class Game(object):
         self.screen = pygame.display.set_mode((800, 600), flags=displayFlags)
         pygame.display.set_caption('Farbenfall v0.1')
         self.clock = pygame.time.Clock()
+        self.eventDict = initUserEvents()
 
-        self.intro = intro(self)
+        self.checkMouseClick = None
+        self.mouseClickStartPos = None
+
+        # TODO: react on window resizing
+        self.background = pygame.Surface(self.screen.get_size()).convert()
+        self.background.fill((255, 255, 255))
+
+        self.intro = intro.Intro(self)
 
         self.spritesDict = {}
 
@@ -43,24 +58,59 @@ class Game(object):
         w_red.moveTo(320, 20)
         w_red.resizeProp(0.5)
 
-        self.allSprites = pygame.sprite.RenderPlain((colorBlock, w_blue, w_green, w_red))
+        self.allGameSprites = pygame.sprite.RenderPlain((colorBlock, w_blue, w_green, w_red))
+
+        self.allActiveSprites = pygame.sprite.RenderPlain()
 
     def run(self):
-
-        pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_HAND)
+        self.showIntro()
 
         while 1:
-            self.intro.render()
             for event in pygame.event.get():
                 if event.type == QUIT:
                     return
                 elif event.type == KEYDOWN and event.key == K_ESCAPE:
                     return
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mouseClickStartPos = pygame.mouse.get_pos()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    if self.checkMouseClick:
+                        self.checkMouseClick(event)
+                elif event.type == self.eventDict['INTRO_CLICKED']:
+                    if event.action == 'PLAY':
+                        self.showGame()
+                    elif event.action == 'ABOUT':
+                        self.showAbout()
 
-            self.allSprites.update()
-            self.allSprites.draw(self.screen)
+            self.screen.blit(self.background, (0, 0))
+            self.allActiveSprites.update()
+            self.allActiveSprites.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
+
+    def resetScene(self):
+        self.checkMouseClick = None
+        self.allActiveSprites.empty()
+
+    def showIntro(self):
+        self.resetScene()
+        pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        self.allActiveSprites.add(self.intro)
+        self.checkMouseClick = self.intro.checkMouseClick
+
+    def showGame(self):
+        self.resetScene()
+        pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_HAND)
+        self.allActiveSprites = self.allGameSprites
+
+    def showAbout(self):
+        self.resetScene()
+        pygame.mouse.set_system_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        # self.allActiveSprites = self.allGameSprites
+
+    def quitGame(self):
+        self.resetScene()
+        exit()
 
 
 if __name__ == "__main__":
